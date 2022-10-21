db3d64a59532b0c9fa8d65b66690339975d4f6ef
barcek <barcek@gmx.com>
2022-10-21 15:25:05 +0100

    feature: revised for route definition with route name data subset (spindle.py)

--- spindle.py
+++ spindle.py
@@ -38,21 +38,36 @@ def make_request(url):
     json_str = res.read().decode('utf-8')
   return loads(json_str)
 
-# define primaries
-
-def locate_value(url, keys=None):
-  # handle any URL subset part
-  if '[' in url or ']' in url:
-    err = SyntaxError(f'Incomplete subset in URL "{url}"')
-    # split URL if subset delimited
-    if not ']' in url or not '[' in url: raise err
-    parts = url.split('[')
-    url = parts[0].strip()
+def parse_source_string(src_str, keys=None):
+  if '[' in src_str or ']' in src_str:
+    err = SyntaxError(f'Incomplete subset in source "{src_str}"')
+    # split src if subset delimited
+    if not ']' in src_str or not '[' in src_str: raise err
+    parts = src_str.split('[')
+    src_str = parts[0].strip()
     # extract keys if notation full
     if '.' != parts[1].strip()[0]: raise err
     keys = [key.strip() for key in parts[1][:-1].split('.')[1:]]
-  value = make_request(url)
-  # restrict data to any subset
+  return (src_str, keys)
+
+# define primaries
+
+def get_value(src_str):
+  # get source and any subset keys
+  source, keys = parse_source_string(src_str)
+  if source not in routes:
+    # assume source is URL and request value
+    value = make_request(source)
+  elif source in values and (not isinstance(values[source], str)\
+    or pending_prefix not in values[source]):
+    # reuse route value previously generated
+    value = values[source]
+  else:
+    # mark route pending and use placeholder
+    placeholder = pending_prefix + source
+    pending[placeholder] = source
+    return placeholder
+  # reduce value to any subset
   if keys: value = pluck_subset(value, keys)
   return value
 
@@ -60,17 +75,7 @@ def gather_values_initial(item):
   # return value for each source URL or placeholder if route pending
   # handle base case
   if isinstance(item, str):
-    # assumed URL: make request and return response
-    if item not in routes: return locate_value(item)
-    # assumed route name:
-    # - value currently available: return data
-    if item in values and (not isinstance(values[item], str)\
-      or pending_prefix not in values[item]):
-      return values[item]
-    # - value unavailable: mark route pending
-    placeholder = pending_prefix + item
-    pending[placeholder] = item
-    return placeholder
+    return get_value(item)
   # recurse for nested items
   if isinstance(item, list):
     return [gather_values_initial(nested_item) for nested_item in item]
@@ -81,7 +86,7 @@ def insert_values_pending(item):
   # return values with each final value substituted for placeholder
   # handle base case
   if isinstance(item, str) and item in pending.keys():
-    return values[pending[item]]
+    return get_value(pending[item])
   # recurse for nested items
   if isinstance(item, list):
     return [insert_values_pending(nested_item) for nested_item in item]
@@ -99,9 +104,9 @@ with open(file, 'r') as f:
 
 # get source URL values
 
-for name, src in source.items():
+for name, src_str in source.items():
   # build values dict with route name as key and source URL data as value
-  values[name] = gather_values_initial(src)
+  values[name] = gather_values_initial(src_str)
 
 for _ in enumerate(pending):
   # update values dict with final values for routes marked pending

55a7aae1df2a4be13490fbf201b4f357489afdfd
barcek <barcek@gmx.com>
2022-10-21 09:34:31 +0100

    feature: revised for route definition with collection nesting, modified naming & added comments (spindle.py)

--- spindle.py
+++ spindle.py
@@ -14,45 +14,56 @@ file = 'spindle.yaml'
 host = 'localhost'
 port = 5912
 
-srcs = {}
-data = {}
+source = {}
+values = {}
 
 # define utilities
 
 def make_request(url):
+  # return parsed JSON response
   req = Request(url)
   with urlopen(req) as res:
     json_str = res.read().decode('utf-8')
   return loads(json_str)
 
+# define primaries
+
+def gather_values_initial(item):
+  # return value for each source URL
+  # handle base case
+  if isinstance(item, str):
+    return make_request(item)
+  # recurse for nested items
+  if isinstance(item, list):
+    return [gather_values_initial(nested_item) for nested_item in item]
+  if isinstance(item, dict):
+    return {key: gather_values_initial(value) for key, value in item.items()}
+
 # parse configuration
 
 with open(file, 'r') as f:
-  # extract config to srcs dict with route name as key and source URL as value
-  conf = f.read()
-  srcs = safe_load(conf)
+  # extract config to source dict with route name as key and source URL as value
+  config = f.read()
+  source = safe_load(config)
 
-# get source URL data
+# get source URL values
 
-for name, src in srcs.items():
-  # build data dict with route name as key and source URL data as value
-  # by making request per source URL and storing parsed response
-  data[name] = make_request(src) if isinstance(src, str)\
-    else [make_request(item) for item in src] if isinstance(src, list)\
-    else dict([[item[0], make_request(item[1])] for item in src.items()])
+for name, src in source.items():
+  # build values dict with route name as key and source URL data as value
+  values[name] = gather_values_initial(src)
 
-# merge data for /all
+# merge values for /all
 
-data['all'] = dict([[items[0], items[1]] for items in data.items()])
+values['all'] = dict([[items[0], items[1]] for items in values.items()])
 
-# serve data by route
+# serve values by route
 
 class TCPRequestHandler(BaseRequestHandler):
 
   def handle(self):
     self.data = self.request.recv(1024).strip()
     route = self.data.decode('utf-8').split('\n')[0].split(' ')[1][1:]
-    res_data = data.get(route) # data or None
+    res_data = values.get(route) # value or None
     res_json = dumps(res_data) if res_data is not None else dumps({'Error': 'Route unavailable'})
     self.request.sendall(bytes(res_json, 'utf-8'))
 
